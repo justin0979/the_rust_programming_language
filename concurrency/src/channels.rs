681253a8fe6523035c807bb9bf0e7a01a01f0372
justin0979 <justinm0979@gmail.com>
2023-10-24 09:53:31 -0500

    move transmitting end of channel into a thread

--- concurrency/src/channels.rs
+++ concurrency/src/channels.rs
@@ -1,6 +1,13 @@
 /// mpsc stands for Multiple Producer, Single Consumer
 use std::sync::mpsc;
+use std::thread;
 
 pub fn channels() {
     let (tx, rx) = mpsc::channel();
+
+    // move transmitting end into thread
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    })
 }
