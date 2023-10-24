4eb0de63d2a2ecc4faf7ab5a63e159eb306658bb
justin0979 <justinm0979@gmail.com>
2023-10-24 10:14:11 -0500

    define fn to show multiple messages being sent to a waiting receiver

--- concurrency/src/channels.rs
+++ concurrency/src/channels.rs
@@ -1,6 +1,7 @@
 /// mpsc stands for Multiple Producer, Single Consumer
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 pub fn channels() {
     let (tx, rx) = mpsc::channel();
@@ -13,7 +14,35 @@ pub fn channels() {
         tx.send(val).unwrap();
     });
 
-    // Receiving the value "hi".
+    // `recv` stands for receive. This method will block the main thread' execution and
+    // wait until a value is sent down the channel.
+    // `recv` returns a Result<T, E>. When the transmitter closes, `recv` will return
+    // an error to signal that no more values will be coming.
+    // `try_recv` does NOT block, but will return a Result<T, E> immediately.
+    // Using `try_recv` is useful if this thread has other work to do while waiting for
+    // messages.
     let received = rx.recv().unwrap();
     println!("Got: {received}");
+}
+
+pub fn send_multiple_messages_and_see_receiver_waiting() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -5,5 +5,6 @@ fn main() {
     // spawn::end_spawn_early();
     // spawn::finish_all_threads_w_join();
     // spawn::move_closures_with_threads();
-    channels::channels();
+    // channels::channels();
+    channels::send_multiple_messages_and_see_receiver_waiting();
 }
