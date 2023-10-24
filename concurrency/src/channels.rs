fc5e901900a68512f1fca87a1b8fad6c912061ff
justin0979 <justinm0979@gmail.com>
2023-10-24 09:57:56 -0500

    setup receiver to receive data from transmitter

--- concurrency/src/channels.rs
+++ concurrency/src/channels.rs
@@ -8,6 +8,12 @@ pub fn channels() {
     // move transmitting end into thread
     thread::spawn(move || {
         let val = String::from("hi");
+        // `send` returns a Result<T, E>, if reciever is already dropped and there is
+        // nowhere to send the data, then Error is returned.
         tx.send(val).unwrap();
-    })
+    });
+
+    // Receiving the value "hi".
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
 }
