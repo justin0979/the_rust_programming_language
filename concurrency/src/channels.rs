fe23f383e8158fe80510cea08970222bcbe5318e
justin0979 <justinm0979@gmail.com>
2023-10-24 10:24:16 -0500

    define fn to show multiple transmitters sending to a single receiver

--- concurrency/src/channels.rs
+++ concurrency/src/channels.rs
@@ -46,3 +46,40 @@ pub fn send_multiple_messages_and_see_receiver_waiting() {
         println!("Got: {received}");
     }
 }
+
+pub fn multiple_producers_single_consumer() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+}
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     // spawn::finish_all_threads_w_join();
     // spawn::move_closures_with_threads();
     // channels::channels();
-    channels::send_multiple_messages_and_see_receiver_waiting();
+    // channels::send_multiple_messages_and_see_receiver_waiting();
+    channels::multiple_producers_single_consumer();
 }
